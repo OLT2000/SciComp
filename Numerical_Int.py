cccd46a74ec9f53e0779626157298db18c43d0fb
mf18131 <mf18131@bristol.ac.uk>
2021-03-23 15:53:00 +0000

    New solve_ode func is very generalised, handles Nd systems, and errors with solver specification

--- Numerical_Int.py
+++ Numerical_Int.py
@@ -4,21 +4,6 @@ from math import log10, log2
 from math import e as exp
 
 
-def main(x_init, t_init, tend, nsteps, h, fn):
-    time = np.linspace(t_init, tend, nsteps)
-    rk_Val_array = solve_ode(x_init, t_init, tend, nsteps,  h, fn, rk4_step)
-    eu_Val_array = solve_ode(x_init, t_init, tend, nsteps, h, fn, euler_step)
-    eu = get_Error(eu_Val_array, time)
-    rk = get_Error(rk_Val_array, time)
-
-
-def get_Error(valarray, tarray):
-    solnarray = np.array(tarray)
-    soln = exp**(solnarray)
-    error_array = abs(valarray-tarray)
-    print(error_array)
-
-
 def function(x, t):
     return x
 
@@ -63,21 +48,46 @@ def solve_to(x1, t1, t2, deltat_max, fn,  step_fn):
     return tempx
 
 
-def solve_ode(x1, tstart, tend, nsteps, h, fn, step_fn):
+def solve_ode(x1, tstart, tend, nsteps, h, fn, solver=None):
+
+    """
+    :param x1: Initial point
+    :param tstart: Start time
+    :param tend: End time
+    :param nsteps: Number of iterative steps taken
+    :param h: Maximum step size
+    :param fn: derivative function being integrated
+    :param solver: Method of solution, Euler for Euler's, and RK4 for RK4, if none, function will ask for input
+    :return: N-D Array of solution points
+    """
+    if solver == None: #if no solver was specified
+        print("Please choose a Solver")
+        solver = input("Euler or RK4")
+
+    if solver == 'Euler':
+        step_fn = euler_step
+    elif solver == 'RK4':
+        step_fn = rk4_step
+    else:
+        print("Invalid Solver")
+        return -1
     tarray = np.linspace(tstart, tend, nsteps)
     nsols = len(tarray)
     x_array = [x1]
-    # x_array = np.array([vars, nsols])
     for i in range(nsols-1):
         t1 = tarray[i]
         t2 = tarray[i+1]
         x_i_1 = solve_to(x_array[-1], t1, t2, h, fn, step_fn)
+        # print("x1", x_i_1)
         x_array.append(x_i_1)
-    soln = np.reshape(x_array, (-1, 2))
+    if type(x1) == int: #If we have a 1d system, ICs will be int not list
+        soln = np.reshape(x_array, (-1, 1))
+    else:
+        soln = np.reshape(x_array, (-1, len(x1)))
     return soln.transpose()
 
 
-def plot_solution(t, x):
+def plot_solution1d(t, x):
     """Produce a figure with timeseries and phasespace plots"""
 
     # Create a figure with two plotting axes side by side:
@@ -85,65 +95,119 @@ def plot_solution(t, x):
     ax1 = fig.add_axes([0.58, 0.15, 0.35, 0.7])
 
     # Timeseries plot
-    # ax1.set_title('Time series: $x$ against $t$')
+    ax1.set_title('Time series: $x$ against $t$')
     ax1.plot(t, x, color='green', linewidth=2, label=r'$x$')
+    # ax1.set_yticks([-1, 0, 1])
+    # ax1.set_xlabel(r'$t$')
+    # ax1.set_xticks([0, np.pi, 2*np.pi, 3*np.pi])
+    # ax1.set_xticklabels([r'$0$', r'$\pi$', r'$2\pi$', r'$3\pi$'])
     ax1.grid()
     ax1.legend()
-
     # Return the figure handle for showing/saving
-    # plt.show()
+    plt.show()
     return fig
 
 
+def plot_solution(t, x, v):
+    """Produce a figure with timeseries and phasespace plots"""
+
+    # Create a figure with two plotting axes side by side:
+    fig = plt.figure(figsize=(6, 3))
+    ax1 = fig.add_axes([0.58, 0.15, 0.35, 0.7])
+    ax2 = fig.add_axes([0.08, 0.15, 0.35, 0.7])
+
+    # Timeseries plot
+    ax1.set_title('Time series: $x, v$ against $t$')
+    ax1.plot(t, x, color='green', linewidth=2, label=r'$x$')
+    ax1.plot(t, v, color='blue', linewidth=2, label=r'$v$')
+    ax1.set_yticks([-1, 0, 1])
+    ax1.set_xlabel(r'$t$')
+    ax1.set_xticks([0, np.pi, 2*np.pi, 3*np.pi])
+    ax1.set_xticklabels([r'$0$', r'$\pi$', r'$2\pi$', r'$3\pi$'])
+    ax1.grid()
+    ax1.legend()
+
+    # Phasespace plot
+    ax2.set_title('Phase space: $v$ against $x$')
+    ax2.plot(x, v, linewidth=2, color='red')
+    ax2.set_xlabel(r'$x$')
+    ax2.set_ylabel(r'$v$', rotation=0)
+    ax2.set_xticks([-1, 0, 1])
+    ax2.set_yticks([-1, 0, 1])
+    ax2.grid()
+
+    # Return the figure handle for showing/saving
+    plt.show()
+    return fig
+
+
+"""
+1D func code
+"""
+# x0 = 1
+# t0 = 0
+# tf = 10
+# iters = 200
+# tarray = np.linspace(t0, tf, iters)
+# result = solve_ode(x0, 0, tf, iters, 0.01, function, 'Euler')
+# x = result
+# plot_solution1d(tarray, x)
+
+"""
+Nd func code"""
 x0 = [0, 1]
 t0 = 0
-tf = 150
-tarray = np.linspace(t0, tf, 200)
-eu = solve_ode(x0, 0, tf, 20, 0.01, f_shm, euler_step)
-rk4 = solve_ode(x0, 0, tf, 200, 0.01, f_shm, rk4_step)
-# print(eu, rk4)
-x = eu[0]
-v = eu[1]
-plt.plot(x, v, color='red')
-# plt.plot(tarray, v, color='green')
-plt.show()
+tf = 50
+iters = 1000
+tarray = np.linspace(t0, tf, iters)
+result = solve_ode(x0, 0, tf, iters, 0.01, f_shm)
+x = result[0]
+v = result[1]
+plot_solution(tarray, x, v)
 
-# for iter in range(tf):
-#     h = (1/2)**iter
-#     rk_Val_array = solve_ode(x0, t0, tf, 200,  h, f_shm, rk4_step)
-#     eu_Val_array = solve_ode(x0, t0, tf, 200, h, f_shm, euler_step)
-#     print(rk_Val_array)
 
-    # eu_Final_val = eu_Val_array[-1]
-    # rk_Final_val = rk_Val_array[-1]
-    # rk_Error = abs(rk_Final_val - exp**tf)
-    # eu_Error = abs(eu_Final_val - exp**tf)
-    # rk_e_array.append(rk_Error)
-    # eu_e_array.append(eu_Error)
-    # h_array.append(h)
+# v = result[1]
 
-# for i in range(len(h_array)):
-#     eu_e_array[i] = log2(eu_e_array[i])
-#     rk_e_array[i] = log2(rk_e_array[i])
-#     h_array[i] = log2(h_array[i])
-
-# fig = plt.figure(figsize=(6, 3))
-# ax1 = fig.add_axes([0.58, 0.15, 0.35, 0.7])
+# # # plt.plot(x, x, color='red')
+# # plt.plot(x, v, color='green')
+# # plt.show()
 #
-# # Timeseries plot
-# # ax1.set_title('Time series: $x$ against $t$')
-# ax1.plot(h_array, eu_e_array, color='green', linewidth=2, label=r'Eulers')
-# ax1.plot(h_array, rk_e_array, label=r'RK4')
-# ax1.grid()
-# ax1.legend()
+# # for iter in range(tf):
+# #     h = (1/2)**iter
+# #     rk_Val_array = solve_ode(x0, t0, tf, 200,  h, f_shm, rk4_step)
+# #     eu_Val_array = solve_ode(x0, t0, tf, 200, h, f_shm, euler_step)
+# #     print(rk_Val_array)
 #
+#     # eu_Final_val = eu_Val_array[-1]
+#     # rk_Final_val = rk_Val_array[-1]
+#     # rk_Error = abs(rk_Final_val - exp**tf)
+#     # eu_Error = abs(eu_Final_val - exp**tf)
+#     # rk_e_array.append(rk_Error)
+#     # eu_e_array.append(eu_Error)
+#     # h_array.append(h)
 #
-# plt.xlabel('log2[h]')
-# plt.ylabel('log2[error]')
-# plt.show()
-# x2 = solve_ode(x0, time, h, function, rk4_step)
-# print(x2)
-# plot_solution(time, x2)
-# plt.show()
+# # for i in range(len(h_array)):
+# #     eu_e_array[i] = log2(eu_e_array[i])
+# #     rk_e_array[i] = log2(rk_e_array[i])
+# #     h_array[i] = log2(h_array[i])
+#
+# # fig = plt.figure(figsize=(6, 3))
+# # ax1 = fig.add_axes([0.58, 0.15, 0.35, 0.7])
+# #
+# # # Timeseries plot
+# # # ax1.set_title('Time series: $x$ against $t$')
+# # ax1.plot(h_array, eu_e_array, color='green', linewidth=2, label=r'Eulers')
+# # ax1.plot(h_array, rk_e_array, label=r'RK4')
+# # ax1.grid()
+# # ax1.legend()
+# #
+# #
+# # plt.xlabel('log2[h]')
+# # plt.ylabel('log2[error]')
+# # plt.show()
+# # x2 = solve_ode(x0, time, h, function, rk4_step)
+# # print(x2)
+# # plot_solution(time, x2)
+# # plt.show()
 
 
