b6c66bedb7ca349b61b30d7abe0b8609a67d4c4a
mf18131 <mf18131@bristol.ac.uk>
2021-04-29 12:52:21 +0100

    Fixed accuracy errors w.r.t timesteps, and array mismatches

--- PDE_solver.py
+++ PDE_solver.py
@@ -70,12 +70,12 @@ def fwdmatrix(max_x, max_t, T, L, pde):
     deltax = x[1] - x[0]            # gridspacing in x
     deltat = t[1] - t[0]            # gridspacing in t
     lmbda = kappa*deltat/(deltax**2)    # mesh fourier number
-    a = lmbda * np.ones(max_x)
-    b = (1-2*lmbda)*np.ones(max_x)
+    a = lmbda * np.ones(max_x-1)
+    b = (1-2*lmbda)*np.ones(max_x-1)
     c = a
     mtrx = np.array([a, b, c])
     pos = [-1, 0, 1]
-    A_FE = sp.sparse.spdiags(mtrx, pos, max_x+1, max_x+1).todense()
+    A_FE = sp.sparse.spdiags(mtrx, pos, max_x-1, max_x-1).todense()
     print("deltax=",deltax)
     print("deltat=",deltat)
     print("lambda=",lmbda)
@@ -83,7 +83,7 @@ def fwdmatrix(max_x, max_t, T, L, pde):
         jarray[i] = pde(x[i]) #Calcs u_I at each x point
     # print(jarray)
     for j in range(0, max_t):
-        jarray1 = np.array(A_FE.dot(jarray))[0]
+        jarray1[1:-1] = np.array(A_FE.dot(jarray[1:-1]))[0]
 
         jarray1[0] = bc1(t[j])
         jarray1[max_x] = bc2(t[j])
@@ -100,12 +100,12 @@ def backwardseuler(max_x, max_t, T, L, pde, verbose = False):
     deltax = x[1] - x[0]            # gridspacing in x
     deltat = t[1] - t[0]            # gridspacing in t
     lmbda = kappa*deltat/(deltax**2)    # mesh fourier number
-    a = -lmbda * np.ones(max_x+1)
-    b = (1+2*lmbda)*np.ones(max_x+1)
+    a = -lmbda * np.ones(max_x-1)
+    b = (1+2*lmbda)*np.ones(max_x-1)
     c = a
     mtrx = np.array([a, b, c])
     pos = [-1, 0, 1]
-    A_BE = sp.sparse.spdiags(mtrx, pos, max_x+1, max_x+1).todense()
+    A_BE = sp.sparse.spdiags(mtrx, pos, max_x-1, max_x-1).todense()
     if verbose == True:
         print("deltax =",deltax)
         print("deltat =",deltat)
@@ -114,7 +114,8 @@ def backwardseuler(max_x, max_t, T, L, pde, verbose = False):
         jarray[i] = pde(x[i]) #Calcs u_I at each x point
     for j in range(0, max_t):
         # print()
-        jarray1 = scipy.sparse.linalg.spsolve(A_BE, jarray)
+        # print(jarray.shape, A_BE.shape)
+        jarray1[1:-1] = scipy.sparse.linalg.spsolve(A_BE, jarray[1:-1])
         # jarray1 = np.linalg.solve(A_BE, jarray)
 
         #set boundary conditions
@@ -127,39 +128,67 @@ def backwardseuler(max_x, max_t, T, L, pde, verbose = False):
 
 
 def cranknicholson(max_x, max_t, T, L, pde, verbose = False):
-    x = np.linspace(0, L, max_x+1)     # mesh points in space
+    x = np.linspace(0, L, max_x+1)
     t = np.linspace(0, T, max_t+1)
-    jarray = np.zeros(x.size)        # u at current time step
+    jarray = np.zeros(x.size)      # u at current time step
     jarray1 = np.zeros(x.size)
     deltax = x[1] - x[0]            # gridspacing in x
     deltat = t[1] - t[0]            # gridspacing in t
     lmbda = kappa*deltat/(deltax**2)    # mesh fourier number
-    a = -(lmbda/2) * np.ones(max_x+1)
-    b = (1+lmbda)*np.ones(max_x+1)
+    a = -(lmbda/2) * np.ones(max_x-1)
+    b = (1+lmbda)*np.ones(max_x-1)
     c = a
-    b_b = (1-lmbda)*np.ones(max_x+1)
+    b_b = (1-lmbda)*np.ones(max_x-1)
 
     mtrx_a = np.array([a, b, c])
     mtrx_b = np.array([-a, b_b, -c])
     pos = [-1, 0, 1]
-    A_CN = sp.sparse.spdiags(mtrx_a, pos, max_x+1, max_x+1).todense()
-    B_CN = sp.sparse.spdiags(mtrx_b, pos, max_x+1, max_x+1).todense()
+    A_CN = sp.sparse.spdiags(mtrx_a, pos, max_x-1, max_x-1).todense()
+    B_CN = sp.sparse.spdiags(mtrx_b, pos, max_x-1, max_x-1).todense()
     if verbose == True:
         print("deltax=",deltax)
         print("deltat=",deltat)
         print("lambda=",lmbda)
     for i in range(0, max_x+1):
         jarray[i] = pde(x[i]) #Calcs u_I at each x point
-    # print(jarray)
+    # print(jarray.reshape(1, -1))
     for j in range(max_t):
-        b_array = np.array(B_CN.dot(jarray))[0]
-        jarray1 = scipy.sparse.linalg.spsolve(A_CN, b_array)
+        b_array = np.array(B_CN.dot(jarray[1:-1]))
+        jarray1[1:-1] = scipy.sparse.linalg.spsolve(A_CN, b_array[0])
         #BCs
         jarray1[0] = 0
         jarray1[max_x] = 0
         # Save u_j at time t[j+1]
         jarray[:] = jarray1[:]
     return x, jarray
+
+
+def fd_method(pde, max_x, max_t, T, L, discretisation = None, verbose = False):
+    x = np.linspace(0, L, max_x+1)     # mesh points in space
+    t = np.linspace(0, T, max_t+1)
+    jarray = np.zeros(x.size)      # u at current time step
+    jarray1 = np.zeros(x.size)
+    deltax = x[1] - x[0]            # gridspacing in x
+    deltat = t[1] - t[0]            # gridspacing in t
+    lmbda = kappa*deltat/(deltax**2)    # mesh fourier number
+    if verbose == True:
+        print("deltax=",deltax)
+        print("deltat=",deltat)
+        print("lambda=",lmbda)
+    for i in range(0, max_x+1):
+        jarray[i] = pde(x[i]) #Calcs u_I at each x point
+    if discretisation == None:
+        print("Please choose a Discretisation")
+        discretisation = input("forward, backward or cn?")
+    if discretisation == 'forward':
+        discretisation = fwdmatrix
+    elif discretisation == 'backward':
+        discretisation = backwardseuler
+    elif discretisation == 'cn':
+        discretisation = cranknicholson
+    else:
+        print("Invalid discretisation\nPlease choose forward, backward, or cn")
+        return -1
 
 
 def finite_diff(pde, max_x, max_t, T, L, discretisation = None):
@@ -191,52 +220,56 @@ def get_slope(error, delta):
 L = 1.0         # length of spatial domain
 T = 0.5
 # Set numerical parameters
-mx = 10   # number of gridpoints in space
-mt = 100   # number of gridpoints in time
+mx = 50   # number of gridpoints in space
+mt = 1000   # number of gridpoints in time
 
 # X, u_j = forwardeuler(mx, mt, T, L)
 # X, u_j = backwardseuler(mx, mt, T, L, u_I)
 # X, u_j = cranknicholson(mx, mt, T, L, u_I)
 # X, u_j = fwdmatrix(mx, mt, T, L)
+
+X, u_j = finite_diff(u_I, mx, mt, T, L)
 xx = np.linspace(0,L,250)
 U_Exact = u_exact(xx, T)
 Errors = []
 Errors1 = []
 delt = []
-for n in range(25):
-    tempdel = T/mt
-    X, u_j = finite_diff(u_I, mx, mt, T, L, discretisation='cn')
-    X1, u_j1 = finite_diff(u_I, mx, mt, T, L, discretisation='backward')
-    Max_Error = abs(np.max(u_j) - np.max(U_Exact))
-    MaxE1 = abs(np.max(u_j1) - np.max(U_Exact))
-    Errors.append(Max_Error)
-    Errors1.append(MaxE1)
-    delt.append(tempdel)
-    mt += 50
-
-
-
-for e in range(len(Errors)):
-    Errors[e] = log2(Errors[e])
-    Errors1[e] = log2(Errors1[e])
-    delt[e] = log2(delt[e])
-
-
-plt.plot(delt, Errors)
-plt.plot(delt, Errors1, 'r-')
-plt.show()
-
-slope = get_slope(Errors, delt)
-slope1 = get_slope(Errors1, delt)
-print("Slope for cn:", slope)
-print("SLope for bwd:", slope1)
+# for n in range(25):
+#     tempdel = T/mt
+#     X, u_j = finite_diff(u_I, mx, mt, T, L, discretisation='cn')
+#     X1, u_j1 = finite_diff(u_I, mx, mt, T, L, discretisation='backward')
+#     Max_Error = abs(np.max(u_j) - np.max(U_Exact))
+#     MaxE1 = abs(np.max(u_j1) - np.max(U_Exact))
+#     Errors.append(Max_Error)
+#     Errors1.append(MaxE1)
+#     delt.append(tempdel)
+#     mt += 50
+#
+#
+# for e in range(len(Errors)):
+#     Errors[e] = log2(Errors[e])
+#     Errors1[e] = log2(Errors1[e])
+#     delt[e] = log2(delt[e])
+#
+#
+# plt.plot(delt, Errors, label='CN')
+# plt.plot(delt, Errors1, 'r-', label= 'BWDs')
+# plt.legend()
+# plt.xlabel('log2(delta_t)')
+# plt.ylabel('log2(abs_error)')
+# plt.show()
+#
+# slope = get_slope(Errors, delt)
+# slope1 = get_slope(Errors1, delt)
+# print("Slope for cn:", slope)
+# print("Slope for bwd:", slope1)
 
 # print(ah)
 #Plot the final result and exact solution
-# plt.plot(X,u_j,'rx',label='num')
+plt.plot(X,u_j,'rx',label='num')
 #
-# plt.plot(xx,u_exact(xx,T),'b-',label='exact')
-# plt.xlabel('X')
-# plt.ylabel('u(x,0.5)')
-# plt.legend(loc='upper right')
-# plt.show()
+plt.plot(xx,u_exact(xx,T),'b-',label='exact')
+plt.xlabel('X')
+plt.ylabel('u(x,0.5)')
+plt.legend(loc='upper right')
+plt.show()
