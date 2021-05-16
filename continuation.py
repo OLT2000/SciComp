c2860e5fc5a7652987820c4ad09b0a6c423eba8c
mf18131 <mf18131@bristol.ac.uk>
2021-05-16 20:03:53 +0100

    completed pseudo arc length implementation on 2d hopf

--- continuation.py
+++ continuation.py
@@ -1,60 +1,112 @@
 import numpy as np
 from scipy.optimize import root
 import matplotlib.pyplot as plt
+from Numerical_Shooting import shooting
+
 
 def cubic_f(x, c):
     return x**3 - x + c
 
 
-#Natural Param Continuation
-#aim to vary c between -2, 2
-c_space = np.linspace(-2, 2, 101)
-c0 = c_space[0]
-c1 = c_space[1]
-del_c = c_space[1] - c_space[0]
-xguess = 5
+def hopf(U, beta):
+    u1, u2 = U
+    du1 = beta*u1 - u2 - u1*(u1**2 + u2**2)
+    du2 = u1 + beta*u2 - u2*(u1**2 + u2**2)
+    return np.array([du1, du2])
 
-#set up v0, v1
-u0 = root(lambda x: cubic_f(x, c0), xguess).x[0]
-u1 = root(lambda x: cubic_f(x, c1), xguess).x[0]
 
-v0 = np.array([c0, u0])
-v1 = np.array([c1, u1])
+beta_space = np.linspace(2, -1, 101)
+delta_beta = beta_space[1] - beta_space[0]
+beta0 = beta_space[0]
+
+T0 = 6.283185307261597
+U0 = np.array([2, 1.41421356e+00, 1.39558408e-09])
+
+
+T1 = 6.283185307262165
+U1 = np.array([1.95, 1.39642400e+00, 1.26064267e-09])
+
+
+V0 = U0
+V1 = U1
+
 #create secant
-secant = v1 - v0
+secant = V1 - V0
+#predict solution
+V_tilda = V1 + secant
+V_array = [V0, V1]
 
-#Predict solution
-v_tilda = v1 + secant
-
-v_array = [v0, v1]
-for n in range(100):
+print("V tilda = ", V_tilda)
+for n in range(1000):
     #define objective func to root
     def objective(v):
-        c, x = v
-        f_v = cubic_f(x, c)
-        PAL = np.dot((v - v_tilda), secant)
-        return np.array([f_v, PAL])
-    solution = root(objective, v_tilda)
+        b, u1, u2 = v
+        f_u = hopf([u1, u2], b)
+        dot_prod = np.dot((v - V_tilda), secant)
+        return np.append(dot_prod, f_u)
+    solution = root(objective, V_tilda)
     v_true = solution.x
-    v_array.append(v_true)
-    secant = v_array[-1] - v_array[-2]
-    v_tilda = v_array[-1] + secant
+    V_array.append(v_true)
+    secant = V_array[-1] - V_array[-2]
+    V_tilda = V_array[-1] + secant
 
-
-
-v_array = np.array(v_array)
+v_array = np.array(V_array)
 c_x = v_array.transpose()
 Cs = c_x[0]
 Xs = c_x[1]
-
-
-
+print(Cs)
+print(Xs)
 
 plt.plot(Cs, Xs)
 plt.title('Pseudo Arc Length continuation')
 plt.xlabel('Parameter [c]')
 plt.ylabel('Equilibrium [x]')
 plt.show()
+
+# #Natural Param Continuation
+# #aim to vary c between -2, 2
+# c_space = np.linspace(-2, 2, 101)
+# c0 = c_space[0]
+# c1 = c_space[1]
+# del_c = c_space[1] - c_space[0]
+# xguess = 5
+#
+# #set up v0, v1
+# u0 = root(lambda x: cubic_f(x, c0), xguess).x[0]
+# u1 = root(lambda x: cubic_f(x, c1), xguess).x[0]
+#
+# v0 = np.array([c0, u0])
+# v1 = np.array([c1, u1])
+# #create secant
+# secant = v1 - v0
+#
+# #Predict solution
+# v_tilda = v1 + secant
+#
+# v_array = [v0, v1]
+# for n in range(100):
+#     #define objective func to root
+#     def objective(v):
+#         c, x = v
+#         f_v = cubic_f(x, c)
+#         PAL = np.dot((v - v_tilda), secant)
+#         return np.array([f_v, PAL])
+#     solution = root(objective, v_tilda)
+#     v_true = solution.x
+#     v_array.append(v_true)
+#     secant = v_array[-1] - v_array[-2]
+#     v_tilda = v_array[-1] + secant
+#
+# v_array = np.array(v_array)
+# c_x = v_array.transpose()
+# Cs = c_x[0]
+# Xs = c_x[1]
+#
+# plt.plot(Cs, Xs)
+# plt.title('Pseudo Arc Length continuation')
+# plt.xlabel('Parameter [c]')
+# plt.ylabel('Equilibrium [x]')
+# plt.show()
 
 
 
