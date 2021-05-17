3c1408ce064f4c95f9770b908da9a882003be6c4
mf18131 <mf18131@bristol.ac.uk>
2021-05-17 19:54:32 +0100

    Implemented Derichlet and Neumann boundaries for crank nicolson, although experiencing inaccuracy with nuemann currently

--- PDE_solver.py
+++ PDE_solver.py
@@ -169,80 +169,87 @@ def backwards_euler_main(max_x, max_t, T, L, pde, bcs, bc_type=None):
         raise ValueError('Boundary conditions must be either dirichlet or neumann')
 
 
-
-def backwardseuler(max_x, max_t, T, L, pde, verbose = False):
-    bc1 = lambda t: 0 #lambda function w.r.t t for boundary condition
-    bc2 = lambda t: 0
+def cn_main(max_x, max_t, T, L, pde, bcs, bc_type=None):
+    bc1 = bcs[0]
+    bc2 = bcs[1]
+    if type(bc1) != types.FunctionType or type(bc2) != types.FunctionType:
+        raise TypeError('Both Boundary Conditions must be of type function (lambda or defined), even if 0')
     x = np.linspace(0, L, max_x+1)     # mesh points in space
     t = np.linspace(0, T, max_t+1)
     jarray = np.zeros(x.size)        # u at current time step
-    jarray1 = np.zeros(x.size)      # u at j+1 timestep
-    bjp1 = np.zeros(jarray[1:-1].size)
-    deltax = x[1] - x[0]            # gridspacing in x
-    deltat = t[1] - t[0]            # gridspacing in t
-    lmbda = kappa*deltat/(deltax**2)    # mesh fourier number
-    a = -lmbda * np.ones(max_x-1)
-    b = (1+2*lmbda)*np.ones(max_x-1)
-    c = a
-    mtrx = np.array([a, b, c])
-    pos = [-1, 0, 1]
-    A_BE = sp.sparse.spdiags(mtrx, pos, max_x-1, max_x-1).todense()
-    if verbose == True:
-        print("deltax =",deltax)
-        print("deltat =",deltat)
-        print("lambda =",lmbda)
-    for i in range(0, max_x+1):
-        jarray[i] = pde(x[i]) #Calcs u_I at each x point
-    for j in range(0, max_t):
-        bjp1[0] = lmbda*bc1(t[j+1])
-        bjp1[-1] = lmbda*bc2(t[j+1])
-        jarray1[1:-1] = scipy.sparse.linalg.spsolve(A_BE, jarray[1:-1]+bjp1)
-
-        #set boundary conditions
-        jarray1[0] = bc1(t[j])
-        jarray1[-1] = bc2(t[j])
-
-        # Save u_j at time t[j+1]
-        jarray[:] = jarray1[:]
-    return x, jarray
-
-
-def cranknicholson(max_x, max_t, T, L, pde, verbose = False):
-    bc1 = lambda time: 0
-    bc2 = lambda time: 0
-    x = np.linspace(0, L, max_x+1)
-    t = np.linspace(0, T, max_t+1)
-    jarray = np.zeros(x.size)      # u at current time step
     jarray1 = np.zeros(x.size)
+
+    #Calculate initial conditions
+    for i in range(0, max_x+1):
+        jarray[i] = pde(x[i])
+
     deltax = x[1] - x[0]            # gridspacing in x
     deltat = t[1] - t[0]            # gridspacing in t
-    lmbda = kappa*deltat/(deltax**2)    # mesh fourier number
-    a = -(lmbda/2) * np.ones(max_x-1)
-    b = (1+lmbda)*np.ones(max_x-1)
-    c = a
-    b_b = (1-lmbda)*np.ones(max_x-1)
+    lmbda = kappa*deltat/(deltax**2)
+    print("Lambda=", lmbda)
+    if bc_type == None:
+        print("Please choose a boundary condition type")
+        bc_type = input("dirichlet, or neumann")
+    if bc_type == 'dirichlet':
+        a = -(lmbda/2) * np.ones(max_x-1)
+        b = (1+lmbda)*np.ones(max_x-1)
+        c = a
+        b_b = (1-lmbda)*np.ones(max_x-1)
 
-    mtrx_a = np.array([a, b, c])
-    mtrx_b = np.array([-a, b_b, -c])
-    pos = [-1, 0, 1]
-    A_CN = sp.sparse.spdiags(mtrx_a, pos, max_x-1, max_x-1).todense()
-    B_CN = sp.sparse.spdiags(mtrx_b, pos, max_x-1, max_x-1).todense()
-    if verbose == True:
-        print("deltax=",deltax)
-        print("deltat=",deltat)
-        print("lambda=",lmbda)
-    for i in range(0, max_x+1):
-        jarray[i] = pde(x[i]) #Calcs u_I at each x point
-    # print(jarray.reshape(1, -1))
-    for j in range(max_t):
-        b_array = np.array(B_CN.dot(jarray[1:-1]))
-        jarray1[1:-1] = scipy.sparse.linalg.spsolve(A_CN, b_array[0])
-        #BCs
-        jarray1[0] = 0
-        jarray1[max_x] = 0
-        # Save u_j at time t[j+1]
-        jarray[:] = jarray1[:]
-    return x, jarray
+        mtrx_a = np.array([a, b, c])
+        mtrx_b = np.array([-a, b_b, -c])
+        pos = [-1, 0, 1]
+        A_CN = sp.sparse.spdiags(mtrx_a, pos, max_x-1, max_x-1).todense()
+        B_CN = sp.sparse.spdiags(mtrx_b, pos, max_x-1, max_x-1).todense()
+        for i in range(0, max_x+1):
+            jarray[i] = pde(x[i]) #Calcs u_I at each x point
+        # print(jarray.reshape(1, -1))
+        for j in range(max_t):
+            b_array = np.array(B_CN.dot(jarray[1:-1]))
+            jarray1[1:-1] = scipy.sparse.linalg.spsolve(A_CN, b_array[0])
+            #BCs
+            jarray1[0] = 0
+            jarray1[max_x] = 0
+            # Save u_j at time t[j+1]
+            jarray[:] = jarray1[:]
+        return x, jarray
+
+    elif bc_type == 'neumann':
+        a = -(lmbda/2) * np.ones(max_x+1)
+        a[-2] = -lmbda
+        b = (1+lmbda)*np.ones(max_x+1)
+        b[0] = 1-lmbda
+        c = -(lmbda/2) * np.ones(max_x+1)
+        c[1] = lmbda
+
+        a_b = (lmbda/2) * np.ones(max_x+1)
+        a_b[-2] = lmbda
+        b_b = (1-lmbda)*np.ones(max_x+1)
+        b_b[0] = 1+lmbda
+        c_b = (lmbda/2) * np.ones(max_x+1)
+        c_b[1] = lmbda
+
+        mtrx_a = np.array([a, b, c])
+        mtrx_b = np.array([a_b, b_b, c_b])
+        pos = [-1, 0, 1]
+        A_CN = sp.sparse.spdiags(mtrx_a, pos, max_x+1, max_x+1).todense()
+        B_CN = sp.sparse.spdiags(mtrx_b, pos, max_x+1, max_x+1).todense()
+        for j in range(max_t):
+            pj = bc1(t[j])
+            pj1 = bc1(t[j+1])
+            qj = bc2(t[j])
+            qj1 = bc2(t[j+1])
+            b_array = np.array(np.dot(B_CN, jarray))[0]
+            bc_vec = np.zeros(jarray.size)
+            bc_vec[1] = -(pj + pj1)
+            # bc_vec[-1] = qj + qj1
+            b_array += (deltax*lmbda*bc_vec)
+            jarray1 = sp.sparse.linalg.spsolve(A_CN, b_array)
+
+            jarray[:] = jarray1[:]
+
+        return x, jarray
+
 
 
 # def fd_method(pde, max_x, max_t, T, L, discretisation = None, verbose = False):
@@ -300,7 +307,7 @@ def get_slope(error, delta):
 
 
 L = 1.0         # length of spatial domain
-T = 0.2
+T = 0.1
 # Set numerical parameters
 mx = 10   # number of gridpoints in space
 mt = 1000   # number of gridpoints in time
@@ -315,7 +322,8 @@ b1test = lambda x: 0
 b2test = lambda x: 0
 
 # X, u_j = forward_euler_main(mx, mt, T, L, u_fx, [b1test, b2test])
-X, u_j = backwards_euler_main(mx, mt, T, L, u_fx, [b1test, b2test], bc_type='neumann')
+X, u_j = cn_main(mx, mt, T, L, u_fx, [b1test, b2test], bc_type='neumann')
+# X, u_j = backwards_euler_main(mx, mt, T, L, u_fx, [b1test, b2test], bc_type='neumann')
 xx = np.linspace(0, L, 250)
 U_Exact = u_exact(xx, T)
 
