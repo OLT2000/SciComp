fb278cdda73be7ba68ffee30f50ea9a56ba36cdd
mf18131 <mf18131@bristol.ac.uk>
2021-05-24 18:57:38 +0100

    made slight changes to performance, removing redunadant lines of code

--- Numerical_Integrator.py
+++ Numerical_Integrator.py
@@ -4,29 +4,31 @@ from math import log10, log2
 from math import e as exp
 
 
-def function(x, t):
-    return x
-
-
-
-
-
-def f_shm(X, t):
-    x, v = X
-    dxdt = v
-    dvdt = -x
-    dXdt = [dxdt, dvdt]
-    return dXdt
-
-
 def euler_step(x_i, h, t_i, fn):
+    """
+    Computes the next iterations of x and t using an Euler step function
+    :param x_i: the initial x value(s) to the euler step
+    :param h: step size
+    :param t_i: initial time step
+    :param fn: the function in x, t computing derivative
+    :return: new x, and t values, one step in forward.
+    """
     deriv = np.array(fn(x_i, t_i))
-    x_i_1 = x_i + h*deriv
-    t_i_1 = t_i + h
+    x_i_1 = x_i + h*deriv #perform euler step
+    t_i_1 = t_i + h #iterate time
     return x_i_1, t_i_1
 
 
 def rk4_step(x_i, h, t_i, fn):
+    """
+    Computes the next iterations of x and t using the fourth order Runge-Kutta step function
+    :param x_i: the initial x value(s) to the euler step
+    :param h: step size
+    :param t_i: initial time step
+    :param fn: the function in x, t to be integrated
+    :return: new x, and t values, one step in forward.
+    """
+    #Compute RK4 integration values
     k1 = np.array(fn(x_i, t_i))
     k2 = np.array(fn(x_i + k1*(h/2), t_i + h/2))
     k3 = np.array(fn(x_i + k2*(h/2), t_i + h/2))
@@ -36,34 +38,39 @@ def rk4_step(x_i, h, t_i, fn):
     return x_i_1, t_i_1
 
 
-def solve_to(x1, t1, t2, deltat_max, fn,  step_fn):
-    Range = t2 - t1
-    tempx = x1
-    tempt = t1
-    full_steps = Range // deltat_max
-    for step in range(int(full_steps)):
-        tempx, tempt = step_fn(tempx, deltat_max, tempt, fn)
-        step += 1
-    if tempt != t2:
-        remainder = t2 - tempt
-        tempx, tempt = step_fn(tempx, remainder, tempt, fn)
-
-    return tempx
-
-
-def solve_ode(x1, tstart, tend, nsteps, h, fn, solver=None):
-
+def solve_to(x1, tspan, deltat_max, fn,  step_fn):
     """
-    :param x1: Initial point
-    :param tstart: Start time
-    :param tend: End time
-    :param nsteps: Number of iterative steps taken
+    Numerically integrates a function from from t1 to t2, in a finite number of steps.
+    During each step, temporary x values are computed, with the final one at time t2 is returned.
+    :param x1: Initial position x values to solve from
+    :param t1: Initial point in time to solve from
+    :param t2: Upper limit in time to the integration
+    :param deltat_max: Maximum time step to take between iterations
+    :param fn: function to be integrated
+    :param step_fn: choice of step function computing next x and t values
+    :return: A single value for x at time t2
+    """
+    t1, t2 = tspan
+    full_steps = int((t2 - t1) // deltat_max) #Whole number of steps available, governed by deltat_max. Converted to Int from float.
+    for step in range(full_steps):
+        x1, t1 = step_fn(x1, deltat_max, t1, fn)
+    if t1 != t2: #Check if we have reached the final t2
+        remainder = t2 - t1
+        x1, tempt = step_fn(x1, remainder, t1, fn)
+    return x1
+
+
+def solve_ode(x1, timespan, nsteps, h, fn, solver=None):
+    """
+
+    :param x1: Initial point in x
+    :param timespan: Timespan over which to integrate
+    :param nsteps: Number of solution points generated
     :param h: Maximum step size
-    :param fn: derivative function being integrated
+    :param fn: Function to be integrated
     :param solver: Method of solution, Euler for Euler's, and RK4 for RK4, if none, function will ask for input
     :return: N-D Array of solution points
     """
-    # print("here", x1)
     if solver == None: #if no solver was specified
         print("Please choose a Solver")
         solver = input("Euler or RK4")
@@ -73,23 +80,21 @@ def solve_ode(x1, tstart, tend, nsteps, h, fn, solver=None):
     elif solver == 'RK4':
         step_fn = rk4_step
     else:
-        print("Invalid Solver")
-        return -1
+        raise KeyError('Invalid solver. Please input Euler for EUler\'s method, or RK4 for Runge-Kutta')
+    tstart, tend = timespan
     tarray = np.linspace(tstart, tend, nsteps)
-    nsols = len(tarray)
     x_array = [x1]
-    for i in range(nsols-1):
+    for i in range(nsteps-1):
         t1 = tarray[i]
         t2 = tarray[i+1]
-        x_i_1 = solve_to(x_array[-1], t1, t2, h, fn, step_fn)
+        x_i_1 = solve_to(x_array[-1], [t1, t2], h, fn, step_fn)
         # print("x1", x_i_1)
         x_array.append(x_i_1)
     if type(x1) == int: #If we have a 1d system, ICs will be int not list
         soln = np.reshape(x_array, (-1, 1))
     else:
         soln = np.reshape(x_array, (-1, len(x1)))
-    # print(soln.transpose())
-    return soln.transpose()
+    return soln.transpose(), tarray
 
 
 def plot_solution1d(t, x):
@@ -160,8 +165,8 @@ def predatorprey(X, t):
 
 def hopf(U, t):
     u1, u2 = U
-    beta = 1
-    sigma = -0.9
+    beta = 2
+    sigma = -1
     du1 = beta*u1 - u2 + sigma*u1*(u1**2 + u2**2)
     du2 = u1 + beta*u2 + sigma*u2*(u1**2 + u2**2)
     return np.array([du1, du2])
@@ -169,8 +174,8 @@ def hopf(U, t):
 
 # if __name__ == "__main__":
 
-
-# Code Testing
+#
+# # Code Testing
 # U0 = [1, -1]
 # t0 = 0
 # tf = 7
@@ -186,15 +191,15 @@ def hopf(U, t):
 """
 Pred-Prey code
 # """
-# xy0 = [0.27015621, 0.27015621]
-# t0 = 0
-# tf = 4000
-# iters = 10000
-# tarray = np.linspace(t0, tf, iters)
-# result = solve_ode(xy0, t0, tf, iters, 0.01, predatorprey, 'RK4')
-# x = result[0]
-# y = result[1]
-# plot_solution(tarray, x, y)
+xy0 = [0.27015621, 0.27015621]
+t0 = 0
+tf = 4000
+span = [t0, tf]
+iters = 10000
+result, tarray = solve_ode(xy0, span, iters, 0.01, predatorprey, 'RK4')
+x = result[0]
+y = result[1]
+plot_solution(tarray, x, y)
 # print(tarray[5368] - tarray[5000], x[5368] - x[5000], y[5368]-y[5000])
 
 
